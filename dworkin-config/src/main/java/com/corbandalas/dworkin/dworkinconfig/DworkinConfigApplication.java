d8a74f194cfa4a8e2d784efb005cca41c6d6fe16
corbandalas <corban.dalas@me.com>
2022-12-14 18:35:18 +0300

    Initial commit

---
+++ dworkin-config/src/main/java/com/corbandalas/dworkin/dworkinconfig/DworkinConfigApplication.java
@@ -0,0 +1,13 @@
+package com.corbandalas.dworkin.dworkinconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DworkinConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DworkinConfigApplication.class, args);
+    }
+
+}
---
+++ dworkin-config/src/test/java/com/corbandalas/dworkin/dworkinconfig/DworkinConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corbandalas.dworkin.dworkinconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DworkinConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
